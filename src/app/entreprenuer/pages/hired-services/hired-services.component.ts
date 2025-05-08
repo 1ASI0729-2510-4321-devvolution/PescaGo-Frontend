8865908ef004936ad5a0eb5aaedb13ebdbed77a8
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-05-08 11:48:04 -0500

    feat(entreprenuer): add module with pages
    Added the carrier module structure including the pages:
    - home
    - request-status
    - search-carriers
    - hired-services

---
+++ src/app/entreprenuer/pages/hired-services/hired-services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hired-services',
+  imports: [],
+  templateUrl: './hired-services.component.html',
+  styleUrl: './hired-services.component.css'
+})
+export class HiredServicesComponent {
+
+}
---
+++ src/app/entreprenuer/pages/search-carriers/search-carriers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-carriers',
+  imports: [],
+  templateUrl: './search-carriers.component.html',
+  styleUrl: './search-carriers.component.css'
+})
+export class SearchCarriersComponent {
+
+}

74234563d2353a2087347a2c639c45f7e914d91a
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-05-08 11:36:12 -0500

    feat(carrier): add module with pages
    Added the carrier module structure including the pages:
    - confirmed-services
    - home
    - requests

---
+++ src/app/carrier/pages/confirmed-services/confirmed-services.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-confirmed-services',
+  imports: [],
+  templateUrl: './confirmed-services.component.html',
+  styleUrl: './confirmed-services.component.css'
+})
+export class ConfirmedServicesComponent {
+
+}

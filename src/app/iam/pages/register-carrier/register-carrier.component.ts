356774dd2023f8602bfe53478dc2e19ff00d29a4
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-05-08 10:52:33 -0500

    feat: add user authentication module
    Added login, signup, and logout functionality using Angular services.

---
+++ src/app/iam/pages/register-carrier/register-carrier.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-carrier',
+  imports: [],
+  templateUrl: './register-carrier.component.html',
+  styleUrl: './register-carrier.component.css'
+})
+export class RegisterCarrierComponent {
+
+}
---
+++ src/app/iam/pages/register-entreprenuer/register-entreprenuer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register-entreprenuer',
+  imports: [],
+  templateUrl: './register-entreprenuer.component.html',
+  styleUrl: './register-entreprenuer.component.css'
+})
+export class RegisterEntreprenuerComponent {
+
+}

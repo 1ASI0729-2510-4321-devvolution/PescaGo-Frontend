e2e7750dabba92dddb9159ea9b0d11f04cb4fcd6
Mariobpv22 <mariobenjamin003@gmail.com>
2025-05-09 15:06:41 -0500

    feature: volver dinamica la tabla de empresas en request-status

--- src/app/entrepreneur/pages/request-status/request-status.component.ts
+++ src/app/entrepreneur/pages/request-status/request-status.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "app-request-status",
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: "./request-status.component.html",
   styleUrl: "./request-status.component.css",
+  standalone: true,
 })
 export class RequestStatusComponent implements OnInit {
   requestData = [

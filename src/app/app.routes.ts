78cf1cebbf79abfc3083219443c7248c43db6a58
IanMQ <129208479+IanMQ@users.noreply.github.com>
2025-05-08 19:05:22 -0500

    feat: add Sign-Up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import {SignInComponent} from "./iam/pages/sign-in/sign-in.component";
+import {SignUpComponent} from "./iam/pages/sign-up/sign-up.component";
 
 export const routes: Routes = [
-    { path: 'sign-in', component: SignInComponent }
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'sign-up', component: SignUpComponent }
 ];

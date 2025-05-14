560cf7fe5606352f8749776efabd1bb4ca8c6549
PaoloBelleza <165170439+PaoloBellezaTelloo@users.noreply.github.com>
2025-05-14 14:24:29 -0500

    feat: add beforeEach

--- src/app/domains/entrepreneur/pages/payment-gateway/payment-gateway.component.spec.ts
+++ src/app/domains/entrepreneur/pages/payment-gateway/payment-gateway.component.spec.ts
@@ -5,4 +5,16 @@ import { PaymentGatewayComponent } from './payment-gateway.component';
 describe('PaymentGatewayComponent', () => {
   let component: PaymentGatewayComponent;
   let fixture: ComponentFixture<PaymentGatewayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PaymentGatewayComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PaymentGatewayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
 });

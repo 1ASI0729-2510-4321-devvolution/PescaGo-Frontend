fe8f3e928df6f187ac2993af81701838ce7b6e06
PaoloBelleza <165170439+PaoloBellezaTelloo@users.noreply.github.com>
2025-05-14 14:23:47 -0500

    feat: add payment-gateway.component.spec.ts

---
+++ src/app/domains/entrepreneur/pages/payment-gateway/payment-gateway.component.spec.ts
@@ -0,0 +1,8 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaymentGatewayComponent } from './payment-gateway.component';
+
+describe('PaymentGatewayComponent', () => {
+  let component: PaymentGatewayComponent;
+  let fixture: ComponentFixture<PaymentGatewayComponent>;
+});

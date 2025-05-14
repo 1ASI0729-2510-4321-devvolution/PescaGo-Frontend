0d59a9819597721187eaa8417f34dafd4391f7de
Mariobpv22 <mariobenjamin003@gmail.com>
2025-05-14 00:16:52 -0500

    Update request-status.component.ts

--- src/app/domains/entrepreneur/pages/request-status/request-status.component.ts
+++ src/app/domains/entrepreneur/pages/request-status/request-status.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from "@angular/core";
 import { RouterLink } from "@angular/router";
 import { CommonModule } from "@angular/common";
-import {ApiService} from "../../../../core/services/api.service";
+import { ApiService } from "../../../../core/services/api.service";
 
 @Component({
   selector: "app-request-status",
-  imports: [RouterLink, CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: "./request-status.component.html",
   styleUrl: "./request-status.component.css",
   standalone: true,
@@ -17,18 +17,20 @@ export class RequestStatusComponent implements OnInit {
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.entrepreneurId = localStorage.getItem('entrepreneurId'); // Recuperar el id
-    console.log('Entrepreneur ID:', this.entrepreneurId);
+    this.entrepreneurId = localStorage.getItem("entrepreneurId"); // Recuperar el id
+    console.log("Entrepreneur ID:", this.entrepreneurId);
 
     if (this.entrepreneurId) {
-      this.apiService.getRequestsByEntrepreneurId(this.entrepreneurId).subscribe({
-        next: (requests) => {
-          this.requestData = requests;
-        },
-        error: (err) => {
-          console.error("Error al cargar los requests:", err);
-        },
-      });
+      this.apiService
+        .getRequestsByEntrepreneurId(this.entrepreneurId)
+        .subscribe({
+          next: (requests) => {
+            this.requestData = requests;
+          },
+          error: (err) => {
+            console.error("Error al cargar los requests:", err);
+          },
+        });
     }
   }
 }

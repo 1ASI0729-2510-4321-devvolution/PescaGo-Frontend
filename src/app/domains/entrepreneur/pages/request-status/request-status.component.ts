d03acd3308d17e369fad6670ea1710d9870d4b10
PaoloBelleza <165170439+PaoloBellezaTelloo@users.noreply.github.com>
2025-05-14 14:32:54 -0500

    feat: add content to export class

--- src/app/domains/entrepreneur/pages/request-status/request-status.component.ts
+++ src/app/domains/entrepreneur/pages/request-status/request-status.component.ts
@@ -11,5 +11,24 @@ import {ApiService} from "../../../../core/services/api.service";
   standalone: true,
 })
 export class RequestStatusComponent implements OnInit {
-  
+  requestData: any[] = [];
+  entrepreneurId: number | null = null;
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.entrepreneurId = parseInt(localStorage.getItem('entrepreneurId') || '0', 10); // Recuperar el id
+    console.log('Entrepreneur ID:', this.entrepreneurId);
+
+    if (this.entrepreneurId) {
+      this.apiService.getRequestsByEntrepreneurId(this.entrepreneurId).subscribe({
+        next: (requests) => {
+          this.requestData = requests;
+        },
+        error: (err) => {
+          console.error("Error al cargar los requests:", err);
+        },
+      });
+    }
+  }
 }

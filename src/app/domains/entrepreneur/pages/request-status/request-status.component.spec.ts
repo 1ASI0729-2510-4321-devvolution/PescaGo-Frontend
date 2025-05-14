559dd00aafa56df4467767db780c740029a9ce05
PaoloBelleza <165170439+PaoloBellezaTelloo@users.noreply.github.com>
2025-05-14 14:33:47 -0500

    feat: add describe to request-status

---
+++ src/app/domains/entrepreneur/pages/request-status/request-status.component.spec.ts
@@ -0,0 +1,10 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+
+import { RequestStatusComponent } from "./request-status.component";
+
+describe("RequestStatusComponent", () => {
+  let component: RequestStatusComponent;
+  let fixture: ComponentFixture<RequestStatusComponent>;
+
+  
+});
